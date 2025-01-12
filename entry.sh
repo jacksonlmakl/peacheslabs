54662e8e34235bd1931728e804475d87db59f18c
Jackson Makl <127900729+jacksonlmakl@users.noreply.github.com>
2025-01-11 20:54:03 -0500

    Update entry.sh

--- entry.sh
+++ entry.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 nohup python auth.py > auth.log 2>&1 &
-nohup python auth_upload.py > auth.log 2>&1 &
+nohup python auth_upload.py > upload.log 2>&1 &
 nohup python auth_app.py > app.log 2>&1 &
